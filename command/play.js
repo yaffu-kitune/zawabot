c486f871c20a133d859d82a2b96f0e0f8bc0efa0
yaffu-kitune <22cc0203@jec.ac.jp>
2023-11-09 23:11:14 +0900

    edit play file

--- command/play.js
+++ command/play.js
@@ -6,68 +6,112 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("play")
     .setDescription("Plays music in your voice channel.")
-    .addStringOption(option =>
+    .addStringOption((option) =>
       option
         .setName("url")
         .setDescription("The URL of the YouTube video or playlist")
-        .setRequired(true)
+        .setRequired(false)
     )
-    .addBooleanOption(option =>
+    .addBooleanOption((option) =>
       option
         .setName("shuffle")
         .setDescription("Turn on shuffle for playlist playback")
         .setRequired(false)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("insert")
+        .setDescription("Insert the song at the top of the queue")
+        .setRequired(false)
     ),
-    async execute({ client, interaction }) {
-      const url = interaction.options.getString("url");
-      const shuffle = interaction.options.getBoolean("shuffle") || false;
+  async execute({ client, interaction }) {
+    const url = interaction.options.getString("url");
+    const shuffle = interaction.options.getBoolean("shuffle") || false;
+    const insert = interaction.options.getBoolean("insert") || false;
+    const voiceChannel = interaction.member.voice.channel;
+
+    if (!voiceChannel) {
+      return interaction.reply("ボイスチャンネルに入ってから呼んでね");
+    }
+    if (!url) {
+      // URLが提供されていない場合、現在の曲を表示します
       const voiceChannel = interaction.member.voice.channel;
-  
       if (!voiceChannel) {
         return interaction.reply("ボイスチャンネルに入ってから呼んでね");
       }
-      await interaction.deferReply({ ephemeral: true }).catch(console.error);
-
-      await interaction.editReply("応答を開始します...").catch(console.error);
       const controller = controllerManager.getController(interaction.guild.id);
-  
-      // ここで応答を保留することができます
-  
-      try {
-        if (play.yt_validate(url) === "playlist") {
-          await controller.handlePlaylist(url, interaction, voiceChannel, shuffle)
-            .then(() => interaction.editReply("プレイリストをキューに追加しました！"))
-            .catch((error) => {
-              console.error(error);
-              interaction.editReply("プレイリストの処理中にエラーが発生しました。");
-            });
-        } else {
-        const videoInfo = await play.video_info(url).catch(error => {
+      const currentSong = controller.currentSong();
+      if (currentSong) {
+        return interaction.reply(`現在再生中の曲: [**${currentSong.title}**](${currentSong.url})`);
+      } else {
+        return interaction.reply("再生中の曲はありません。");
+      }
+    }
+    
+    await interaction.deferReply({ ephemeral: true }).catch(console.error);
+
+    await interaction.editReply("応答を開始します...").catch(console.error);
+    const controller = controllerManager.getController(interaction.guild.id);
+
+    // ここで応答を保留することができます
+
+    try {
+      if (play.yt_validate(url) === "playlist") {
+        await controller
+          .handlePlaylist(url, interaction, voiceChannel, shuffle)
+          .then(() =>
+            interaction.editReply("プレイリストをキューに追加しました！")
+          )
+          .catch((error) => {
+            console.error(error);
+            interaction.editReply(
+              "プレイリストの処理中にエラーが発生しました。"
+            );
+          });
+      } else {
+        const videoInfo = await play.video_info(url).catch((error) => {
           console.error(error);
-          interaction.editReply("動画情報の取得に失敗しました。URLが正しいか確認してください。");
+          interaction.editReply(
+            "動画情報の取得に失敗しました。URLが正しいか確認してください。"
+          );
         });
         if (!videoInfo) return;
-        
-        const thumbnail = videoInfo.video_details.thumbnails[0]?.url || 'デフォルトのサムネイルURL';
+
+        const thumbnail =
+          videoInfo.video_details.thumbnails[0]?.url ||
+          "デフォルトのサムネイルURL";
         const songDetails = {
           title: videoInfo.video_details.title,
           url: videoInfo.video_details.url,
           duration: videoInfo.video_details.durationInSec,
           thumbnail,
         };
-        controller.queue.add(songDetails);
+        if (insert) {
+          controller.insertSong(songDetails);
+          interaction.editReply(`キューの先頭に追加しました: [**${songDetails.title}**](${songDetails.url})`);
+        } else {
+          controller.queue.add(songDetails);
+        }
 
         if (!controller.queue.playing) {
           await controller.playSong(interaction.guild, voiceChannel);
-          interaction.editReply(`今再生中: [**${songDetails.title}**](${songDetails.url})`);
+          interaction.editReply(
+            `今再生中: [**${songDetails.title}**](${songDetails.url})`
+          );
         } else {
-          interaction.editReply(`キューに追加しました: [**${songDetails.title}**](${songDetails.url})`);
+          interaction.editReply(
+            `キューに追加しました: [**${songDetails.title}**](${songDetails.url})`
+          );
         }
       }
-      await interaction.editReply("プレイリストをキューに追加しました！").catch(console.error);
+      await interaction
+        .editReply("プレイリストをキューに追加しました！")
+        .catch(console.error);
     } catch (error) {
       console.error(error);
-      await interaction.followUp({ content: "エラーが発生しました。", ephemeral: true }).catch(console.error);
+      await interaction
+        .followUp({ content: "エラーが発生しました。", ephemeral: true })
+        .catch(console.error);
     }
   },
 };
--- src/lib/MusicManager.js
+++ src/lib/MusicManager.js
@@ -7,6 +7,7 @@ const {
   getVoiceConnection,
 } = require("@discordjs/voice");
 
+
 class MusicQueue {
   constructor() {
     this.queue = [];
@@ -16,15 +17,18 @@ class MusicQueue {
 
   add(song) {
     this.queue.push(song);
-    console.log(`曲が追加されました: ${song.title}, キューの長さ: ${this.queue.length}`);
+    console.log(
+      `曲が追加されました: ${song.title}, キューの長さ: ${this.queue.length}`
+    );
   }
-  
+
   next() {
     return this.queue.shift();
   }
 }
 
 class MusicController {
+  
   constructor() {
     this.queue = new MusicQueue();
     this.audioPlayer = null;
@@ -34,15 +38,17 @@ class MusicController {
 
   async handlePlaylist(url, interaction, voiceChannel, shuffle) {
     console.log("プレイリスト情報の取得を開始...");
-    const playlist = await play.playlist_info(url, { incomplete: true }).catch(console.error);
+    const playlist = await play
+      .playlist_info(url, { incomplete: true })
+      .catch(console.error);
     if (!playlist) {
       console.log("プレイリスト情報が取得できませんでした。");
       return;
     }
-  
+
     let songs = [];
     let videoArray = playlist.videos; // すでに取得されたビデオの配列を使用
-  
+
     for (const video of videoArray) {
       let videoInfo = await play.video_info(video.url).catch(console.error);
       if (!videoInfo) continue; // video_infoが取得できない場合はスキップ
@@ -55,12 +61,12 @@ class MusicController {
           "デフォルトのサムネイルURL",
       });
     }
-  
+
     // シャッフルオプションが有効な場合はシャッフルを適用
     if (shuffle) {
       songs = this.shuffleArray(songs);
     }
-  
+
     // キューに曲を追加
     for (const song of songs) {
       this.queue.add(song);
@@ -68,13 +74,35 @@ class MusicController {
     }
     console.log(`追加される曲の数: ${songs.length}`);
 
-  
     // 再生を開始
     if (!this.queue.playing) {
       await this.playSong(interaction.guild, voiceChannel);
     }
   }
-  
+
+  currentSong() {
+    
+
+    if (this.isPlaying && this.queue.length > 0) {
+      return this.queue[0]; // 仮にキューの最初の曲が現在再生中とする
+    } else {
+      console.log("再生中の曲はありません。");
+      return null;
+    }
+  }
+
+  insertSong(song, position) {
+    // 位置がキューの範囲内であるか確認し、適切な位置に挿入
+    if (position < 0) {
+      position = 0;
+    } else if (position > this.queue.queue.length) {
+      position = this.queue.queue.length;
+    }
+    this.queue.queue.splice(position, 0, song);
+    console.log(
+      `曲が挿入されました: ${song.title}, キューの長さ: ${this.queue.queue.length}`
+    );
+  }
 
   shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -114,7 +142,7 @@ class MusicController {
       const player = createAudioPlayer();
       this.audioPlayer = player;
 
-      player.on('error', error => {
+      player.on("error", (error) => {
         console.error(`Error occurred in the audio player: ${error.message}`);
         this.playNext().catch(console.error);
       });
@@ -141,24 +169,23 @@ class MusicController {
       console.log("ボイスチャンネルに接続していません");
       return false;
     }
-  
+
     const connection = getVoiceConnection(this.currentGuild.id);
     if (!connection) {
       console.log("ボイスチャンネルに接続していません");
       return false;
     }
-  
+
     const dispatcher = connection.state.subscription?.player;
     if (!dispatcher) {
       console.log("再生中のトラックはありません");
       return false;
     }
-  
+
     dispatcher.stop(); // 現在の曲を停止する
-  
+
     return true;
   }
-  
 
   async playNext() {
     if (this.queue.queue.length > 0) {
@@ -169,7 +196,6 @@ class MusicController {
       this.closeConnectionAfterTimeout(this.currentGuild.id, 30 * 1000);
     }
   }
-
 
   closeConnectionAfterTimeout(guildId, timeout) {
     setTimeout(() => {
